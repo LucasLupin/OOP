854b906311193d0b2f905172fd7f9df25845217b
Lucas Kramhöft <110811527+LucasLupin@users.noreply.github.com>
2022-09-01 08:16:29 +0200

    Add files via upload
    PersonObjekter

---
+++ PersonObjekter/PersonObjekter/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace PersonObjekter
+{
+    internal class Person
+    {
+        private string email;
+        private string password;
+        private string name;
+
+        public DateTime DoB { get; set; }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == "" || value.All(Char.IsDigit)) name = null;
+                else name = value;
+            }
+        }
+
+        public int Age { get { return GetAge(); } }
+        public int GetAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DoB.Year;
+            if (today < new DateTime(today.Year, DoB.Month, DoB.Day)) age--;
+            return age;
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value.Contains("@") && value.Contains("."))
+                {
+                    email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Email skal havde @ og .");
+                    email = null;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                bool isNumber = false;
+                bool isLower = false;
+                bool isUpper = false;
+
+
+                if (value.Length < 6)
+                {
+                    Console.WriteLine("Skal mindst havde 6 karaktere");
+                    password = null;
+                }
+
+                foreach (char c in value)
+                {
+                    if (char.IsUpper(c)) { isUpper = true;  }
+                    if (char.IsLower(c)) { isLower = true; }
+                    if (char.IsDigit(c)) { isNumber = true; }
+                }
+
+                if(isUpper == true && isLower == true && isNumber == true)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Koden skal inde holde små, store bogstaver og Tal");
+                    password = null;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ PersonObjekter/PersonObjekter/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace PersonObjekter
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            Person myPerson = new Person();
+
+            Navn(myPerson);
+            Password(myPerson);
+            Email(myPerson);
+            DoBAge(myPerson);
+        }
+
+
+        static void Navn(Person myPerson)
+        {
+            do
+            {
+                Console.WriteLine("Skriv dit navn her");
+                string? nameInput = Console.ReadLine();
+                myPerson.Name = nameInput;
+            }
+            while (myPerson.Name == null);
+        }
+
+         static void Email(Person myPerson)
+        {
+            do
+            {
+              Console.WriteLine("Skriv din Email her");
+              string? EmailInput = Console.ReadLine();
+              myPerson.Email = EmailInput;
+            }
+            while (myPerson.Email == null);
+
+            Console.Clear();
+            Console.WriteLine($"Hej, Dit Navn er {myPerson.Name}");
+            Console.WriteLine($"Din Mail er {myPerson.Email}");
+            Console.WriteLine($"Dit password er {myPerson.Password}");
+        }
+
+        static void DoBAge(Person myPerson)
+        { 
+            myPerson.DoB = new DateTime(2000, 1, 1);
+            Console.WriteLine($"Din alder er {myPerson.GetAge()} ");
+        }
+
+        static void Password(Person myPerson)
+        {
+            do
+            {
+                Console.WriteLine("Skriv dit password her ");
+                string? passwordInput = Console.ReadLine();
+                myPerson.Password = passwordInput;
+            }
+            while (myPerson.Password == null);
+
+      
+        } 
+
+    }
+}
